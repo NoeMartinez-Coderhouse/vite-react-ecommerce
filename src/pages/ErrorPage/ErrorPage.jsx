60f438de715a5dabd7e14e34e25ab03d6243c5c9
florencia noelia martinez <noe.martinez.coderhouse@gmail.com>
2024-01-04 01:51:34 -0300

    add redirect from error page to home

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/");
+        }, 3000);
+    });
+
     return (
         <div>
             <h1>Error Page</h1>
             <p>404</p>
+            <p>Usted será redireccionado en 3 segundos a la home page...
+            </p>
         </div>
     )
 };
